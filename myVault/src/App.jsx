8f156a5f76c581c9006924ce8b3e0bb123ca123b
DawsonSamuelYaw <dawsonsam05595@gmail.com>
2026-02-22 18:36:08 +0000

    Installation of React, Tailwind and creation of Components(Sidebar,Header,file explorer, properties)

---
+++ myVault/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/header"
+import Sidebar from "./components/sidebar"
+import FileExplorer from "./components/fileExplorer"
+import Properties from "./components/Properties"
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Sidebar/>
+    <FileExplorer/>
+    <Properties/>
+    </>
+  )
+}
+
+export default App
